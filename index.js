87f118d93299fcdc906c9d7ffead3d971ed424ee
opariffazman <opariffazman@gmail.com>
2022-09-24 18:50:03 +0800

    debug json array

--- index.js
+++ index.js
@@ -50,26 +50,34 @@ app.post('*', async (req, res) => {
   let filename = req.path.slice(1)
   let current = [];
 
-  const buffer = Buffer.concat(
-    await (
-      await s3Client
-        .send(new GetObjectCommand({
-          Bucket: process.env.BUCKET,
-          Key: filename,
-        }))
-    ).Body.toArray()
-  )
+  try {
+    let s3File = await s3.getObject({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    }).promise()
 
-  console.log(buffer)
-  // await s3.putObject({
-  //   Body: JSON.stringify(current),
-  //   Bucket: process.env.BUCKET,
-  //   Key: filename,
-  // }).promise()
+    current = s3File.Body.toString()
+    console.log(current)
+  } catch (error) {
+    if (error.code === 'NoSuchKey') {
+      console.log(`No such key ${filename}`)
+      res.sendStatus(404).end()
+    } else {
+      console.log(error)
+      res.sendStatus(500).end()
+    }
+  }
 
-  // res.set('Content-type', 'application/json')
-  // res.send('json updated')
-  // res.send(current);
+  // current['slides'].push(req.body);
+  await s3.putObject({
+    Body: JSON.stringify(current),
+    Bucket: process.env.BUCKET,
+    Key: filename,
+  }).promise()
+
+  res.set('Content-type', 'application/json')
+  res.send('json updated')
+  res.send(current);
 })
 
 // curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
